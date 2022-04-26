a4dfb299684c13efe7950f82d006b8e3d66ca2e5
aylin <asg195@scarletmail.rutgers.edu>
2022-04-26 10:55:19 -0400

    files reuploaded

---
+++ src/components/Form.js
@@ -0,0 +1,17 @@
+//Unused function - meant to condense form. 
+
+function Form() {
+    return (
+        <form>
+            <label>
+                <p>Add Bird</p>
+                <input type="text" />
+            </label>
+                <div>
+                    <button type="submit">Add</button>
+                </div>
+        </form>
+    )
+}
+
+export default Form;
